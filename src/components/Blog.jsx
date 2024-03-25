6297ec1a0a571edc4a1bf02bdae6e08fa0b2a9f1
VerneriLahteenoja <verneri.lahteenoja@gmail.com>
2024-03-25 18:50:20 +0200

    add a button to show more info

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,33 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return (
+  <div style={blogStyle}>
+    <div>
+      {blog.title} {blog.author}
+      {' '}
+      <button type="button" onClick={() => setVisible(!visible)}>{visible ? 'Hide' : 'View'}</button>
+    </div>
+    {visible && <div>
+      {blog.url}
+      <div>
+        likes {blog.likes} {' '}
+        <button type="button">like</button>
+      </div>
+      <div>{blog.user.name}</div>
+    </div>
+    }
+  </div>
+)}
 
 export default Blog
