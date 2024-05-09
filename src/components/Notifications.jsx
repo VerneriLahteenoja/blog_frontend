e689dd319a87385f7f97e89998a7d9b80dd2d76a
VerneriLahteenoja <verneri.lahteenoja@gmail.com>
2024-05-10 01:04:56 +0300

    Create NotificationContext provider

--- src/components/Notifications.jsx
+++ src/components/Notifications.jsx
@@ -1,14 +1,32 @@
+import { createContext, useReducer } from 'react'
 
-const Notification = ({ message, success }) => {
-  const isSuccessful = success ? 'success' : 'error'
-  if (message === null) {
-    return null
+// Reducer to handle notification states
+// type determines message flavor
+// payload will be the notification shown
+const notificationReducer = (state, action) => {
+  switch (action.type) {
+    case 'SUCCESS':
+      return action.payload
+    case 'ERROR':
+      return action.payload
+    default:
+      return ''
   }
+}
+
+const NotificationContext = createContext()
+
+export const NotificationContextProvider = (props) => {
+  const [message, messageDispatch] = useReducer(notificationReducer, '')
+  console.log(message)
+  const isSuccessful = message === 'Blog added!' ? 'success' : 'error'
+
   return (
-    <div className={isSuccessful}>
-      {message}
-    </div>
+    <NotificationContext.Provider value={[message, messageDispatch]}>
+      {message && <div className={isSuccessful}>{message}</div>}
+      {props.children}
+    </NotificationContext.Provider>
   )
 }
 
-export default Notification
+export default NotificationContext
