450c96b8387e4b86646503891c8e8ffbd91256f3
VerneriLahteenoja <verneri.lahteenoja@gmail.com>
2024-05-09 22:57:53 +0300

    add prettier formatting configuration

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -9,28 +9,27 @@ const Blog = ({ blog, username, updateBlog, deleteBlog }) => {
     paddingLeft: 2,
     border: 'solid',
     borderWidth: 1,
-    marginBottom: 5
+    marginBottom: 5,
   }
 
   const handleLike = async () => {
     try {
-      await updateBlog(
-        blog.id,
-        {
-          title: blog.title,
-          author: blog.author,
-          likes: blog.likes + 1,
-          url: blog.url,
-          user: blog.user.id
-        }
-      )
+      await updateBlog(blog.id, {
+        title: blog.title,
+        author: blog.author,
+        likes: blog.likes + 1,
+        url: blog.url,
+        user: blog.user.id,
+      })
     } catch (exception) {
       console.log(exception)
     }
   }
 
   const handleDelete = async () => {
-    if (window.confirm(`Do you want to remove ${blog.title} by ${blog.author}?`)) {
+    if (
+      window.confirm(`Do you want to remove ${blog.title} by ${blog.author}?`)
+    ) {
       await deleteBlog(blog.id)
     }
   }
@@ -38,34 +37,38 @@ const Blog = ({ blog, username, updateBlog, deleteBlog }) => {
   return (
     <div style={blogStyle}>
       <div>
-        {blog.title} by {blog.author}
-        {' '}
-        <button type="button" onClick={() => setVisible(!visible)}>{visible ? 'Hide' : 'View'}</button>
+        {blog.title} by {blog.author}{' '}
+        <button type="button" onClick={() => setVisible(!visible)}>
+          {visible ? 'Hide' : 'View'}
+        </button>
       </div>
-      {visible && <div>
-        {blog.url}
-        <div data-testid="likes">
-        likes {blog.likes} {' '}
-          <button type="button" onClick={handleLike}>like</button>
-        </div>
-        <div>{blog.user.name}</div>
+      {visible && (
+        <div>
+          {blog.url}
+          <div data-testid="likes">
+            likes {blog.likes}{' '}
+            <button type="button" onClick={handleLike}>
+              like
+            </button>
+          </div>
+          <div>{blog.user.name}</div>
 
-        {username === blog.user.username &&
-        <button
-          type="button"
-          onClick={handleDelete}>
-          remove
-        </button>}
-      </div>
-      }
+          {username === blog.user.username && (
+            <button type="button" onClick={handleDelete}>
+              remove
+            </button>
+          )}
+        </div>
+      )}
     </div>
-  )}
+  )
+}
 
 Blog.propTypes = {
   blog: PropTypes.object,
   username: PropTypes.string.isRequired,
   updateBlog: PropTypes.func.isRequired,
-  deleteBlog: PropTypes.func.isRequired
+  deleteBlog: PropTypes.func.isRequired,
 }
 
 export default Blog
